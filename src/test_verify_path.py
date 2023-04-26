244d6b60592e3bbdef77e4d25d894d8e663f82cd
Oaklight <pding.dp@foxmail.com>
2023-04-27 02:54:14 +0800

    update file names

--- src/test_verify_path.py
+++ src/test_verify_path.py
@@ -1,10 +1,10 @@
 from digraph import build_graph_from_file, plot_graph, verify_path, parse_path
 
-g = build_graph_from_file('../data/Zork_Locations.txt',
-                          '../data/Zork_opposite_directions.txt')
+g = build_graph_from_file('../data/zork1.map',
+                          '../data/zork1.actions')
 plot_graph(g)
 
 # verify path test
-srcNode, dstNode, paths2verify = parse_path("../data/Zork_paths2verify.txt")
+srcNode, dstNode, paths2verify = parse_path("../data/zork1.verify")
 result = verify_path(g, srcNode, dstNode, paths2verify)
 print(f"VERIFIED RESULT: \033[1m{result}\033[0m")
