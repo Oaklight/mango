1ee5c279f386bcb18c269299e0d5e2cdd66bd8d1
Peng Ding <pding.dp@foxmail.com>
2023-04-28 13:52:38 +0800

    update map reverser to support different files

--- src/map_reverser.py
+++ src/map_reverser.py
@@ -1,3 +1,21 @@
+import argparse
 from digraph import intuitive_reverse_map
+from utils import printColor, inputColor, buildMap
 
-intuitive_reverse_map('../data/zork1.map', '../data/zork1.actions')
+
+# argparse to read in the map and actions files
+# call intuitive_reverse_map with the map and actions files
+# write the output to a file
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--map', type=str, default='../data/zork1.map')
+parser.add_argument('--actions', type=str, default='../data/zork1.actions')
+args = parser.parse_args()
+
+printColor(f"processing map: {args.map}, actions: {args.actions}", 'b')
+# prompt to confirm before continue
+confirm = inputColor("Continue? (y/n) ", 'b', inline=True)
+if confirm == "y":
+    intuitive_reverse_map(args.map, args.actions)
+else:
+    printColor("Aborted!", 'b')
