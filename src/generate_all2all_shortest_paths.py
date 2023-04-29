17491ac3da8bc2cd6da1ea72d357fceab79e7ee5
Peng Ding <pding.dp@foxmail.com>
2023-04-30 00:41:01 +0800

    build map using both forward and backward map

--- src/generate_all2all_shortest_paths.py
+++ src/generate_all2all_shortest_paths.py
@@ -1,13 +1,14 @@
 import argparse
 import itertools
 
-from digraph import build_graph_from_file, get_shortest_path, plot_graph, print_path
+from digraph import build_graph_from_file, get_shortest_path, plot_graph, print_path, build_graph_from_file_with_reverse
 from md2json import md2json
 from utils import inputColor, printColor
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--map", "-m", type=str, default="../data/zork1.map")
 parser.add_argument("--actions", "-a", type=str, default="../data/zork1.actions")
+parser.add_argument("--reverse_map", type=str, default="../data/zork1.map.reversed")
 parser.add_argument("--output_dir", "-odir", type=str, default="../data/")
 args = parser.parse_args()
 args.output_path = (
@@ -15,12 +16,19 @@ args.output_path = (
     + args.map.split("/")[-1].split(".")[0]
     + ".all2all_shortest_paths.md"
 )
+if args.actions == 'None' or args.actions == 'none' or args.actions == '':
+    args.actions = None
+if args.reverse_map == 'None' or args.reverse_map == 'none' or args.reverse_map == '':
+    args.reverse_map = None
 
 printColor(f"building map: {args.map}, actions: {args.actions}", "b")
 # prompt to confirm before continue
 confirm = inputColor("Continue? (y/n) ", "b", inline=True)
 if confirm == "y":
-    g = build_graph_from_file(args.map, args.actions)
+    if args.reverse_map:
+        g = build_graph_from_file_with_reverse(args.map, args.reverse_map, args.actions)
+    else:
+        g = build_graph_from_file(args.map, args.actions)
 else:
     printColor("Aborted!", "b")
     exit(1)
