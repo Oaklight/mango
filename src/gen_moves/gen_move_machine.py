c3d7768c2b70f114be0f454f58bc390ccda18086
Peng Li <ruhao9805@gmail.com>
2023-05-03 17:01:54 +0800

    update gen_move_machine.py

--- src/gen_moves/gen_move_machine.py
+++ src/gen_moves/gen_move_machine.py
@@ -1,6 +1,8 @@
 from jericho import * # https://jericho-py.readthedocs.io/en/latest/index.html
-from argparse import ArgumentParser
-
+import os
+import argparse
+from tqdm import tqdm
+from jericho import *
 
 direction_abbrv_dict = {'e': 'east', 'w': 'west', 'n': 'north', 's': 'south',
                         'ne': 'northeast', 'nw': 'northwest', 'se': 'southeast', 'sw': 'southwest',
@@ -20,28 +22,24 @@ opposite_direction_dict = {
     'down': 'down'
 }
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument("--jericho_path", '-j', type=str, default="z-machine-games-master/jericho-game-suite")
-    parser.add_argument("--output_dir", '-odir', type=str, default="./maps")
-    args = parser.parse_args()
-
-    game_name = 'zork1.z5'
-    max_steps = 70
+def gen_move_machine(args):
+    game_name = args.game_name
+    max_steps = args.max_steps
     print ('Game: {}, Max steps: {}'.format(game_name, max_steps))
 
     # env
     env = FrotzEnv("{}/{}".format(args.jericho_path, game_name))
-    initial_observation, info = env.reset()
+    env.reset()
     location_before = env.get_player_location().name.strip().lower()
     location_before_id = env.get_player_location().num
 
     # walkthrough
-    walkthrough = env.get_walkthrough()
-    walkthrough = [direction_abbrv_dict[item.lower()] if item.lower() in direction_vocab_abbrv else item.lower() for item in walkthrough]
+    walkthrough_raw = env.get_walkthrough()
+    abbrv_dict = jericho.defines.ABBRV_DICT
+    walkthrough = [abbrv_dict[item.lower()] if item.lower() in abbrv_dict else item.lower() for item in walkthrough_raw]
 
     map_list = []
-    map_reversed_list = []
+    # map_reversed_list = []
     move_list = []
     for step_idx, act in enumerate(walkthrough[:max_steps]):
         observation, reward, done, info = env.step(act)
@@ -57,79 +55,91 @@ def main():
                 'step_num': step_idx + 1
             })
             move_list.append(act)
-            # reverse
-            valid_actions = env.get_valid_actions()
-            if act in direction_vocab:
-                if opposite_direction_dict[act] in valid_actions:
-                    desc = 'None'
-                else:
-                    obsrv_splitted = observation.split('\n')
-                    if len(obsrv_splitted) == 1:
-                        desc = obsrv_splitted[0]
-                    else:
-                        desc = '{} || {}'.format(obsrv_splitted[0], "".join(obsrv_splitted[1:]))
-
-                map_reversed_list.append({
-                    'location_before': location_after,
-                    'location_before_id': location_after_id,
-                    'act': opposite_direction_dict[act],
-                    'location_after': location_before,
-                    'location_after_id': location_before_id,
-                    'step_num': step_idx + 1,
-                    'desc': desc
-                }
-                )
-
-            else:
-                map_reversed_list.append({
-                    'location_before': None,
-                    'location_before_id': None,
-                    'act': None,
-                    'location_after': None,
-                    'location_after_id': None,
-                    'step_num': None,
-                    'desc': None
-                }
-                )
+            # # reverse
+            # valid_actions = env.get_valid_actions()
+            # if act in direction_vocab:
+            #     if opposite_direction_dict[act] in valid_actions:
+            #         desc = 'None'
+            #     else:
+            #         obsrv_splitted = observation.split('\n')
+            #         if len(obsrv_splitted) == 1:
+            #             desc = obsrv_splitted[0]
+            #         else:
+            #             desc = '{} || {}'.format(obsrv_splitted[0], "".join(obsrv_splitted[1:]))
+            #
+            #     map_reversed_list.append({
+            #         'location_before': location_after,
+            #         'location_before_id': location_after_id,
+            #         'act': opposite_direction_dict[act],
+            #         'location_after': location_before,
+            #         'location_after_id': location_before_id,
+            #         'step_num': step_idx + 1,
+            #         'desc': desc
+            #     }
+            #     )
+            # else:
+            #     map_reversed_list.append({
+            #         'location_before': None,
+            #         'location_before_id': None,
+            #         'act': None,
+            #         'location_after': None,
+            #         'location_after_id': None,
+            #         'step_num': None,
+            #         'desc': None
+            #     }
+            #     )
 
             location_before = location_after
             location_before_id = location_after_id
 
-    output_file = '{}/{}.map.machine'.format(args.output_dir,game_name.split('.')[0])
+    output_dir = args.output_dir + '/' + game_name.split('.')[0]
+    if os.path.exists(output_dir) == False:
+        os.makedirs(output_dir)
+
+    output_file = '{}/{}.map.machine'.format(output_dir,game_name.split('.')[0])
     with open(output_file,'w', encoding='utf-8') as fout:
         for item in map_list:
             fout.write('{} (obj{}) --> {} --> {} (obj{}), step {}\n'.format(item['location_before'],
-                                                                              item['location_before_id'],
-                                                                              item['act'],
-                                                                              item['location_after'],
-                                                                              item['location_after_id'],
-                                                                              item['step_num']))
+                                                                            item['location_before_id'],
+                                                                            item['act'],
+                                                                            item['location_after'],
+                                                                            item['location_after_id'],
+                                                                            item['step_num']))
     print ("Saved to {}".format(output_file))
 
-    output_file = '{}/{}.map.machine.reversed'.format(args.output_dir, game_name.split('.')[0])
-    with open(output_file,'w', encoding='utf-8') as fout:
-        for item in map_reversed_list:
-            if item['act'] != None:
-                fout.write('{} (obj{}) --> {} --> {} (obj{}), step {}, desc: {}\n'.format(item['location_before'],
-                                                                                            item['location_before_id'],
-                                                                                            item['act'],
-                                                                                            item['location_after'],
-                                                                                            item['location_after_id'],
-                                                                                            item['step_num'],
-                                                                                            item['desc']))
-            else:
-                fout.write('\n')
-
-    outfile = '{}/{}.walkthrough.moves.{}'.format(args.output_dir, game_name.split('.')[0], max_steps)
+    outfile = '{}/{}.walkthrough.moves.{}'.format(output_dir, game_name.split('.')[0], max_steps)
     with open(outfile, 'w', encoding='utf-8') as fout:
         for sample in set(move_list).union(set(direction_vocab)) :
             fout.write('{}\n'.format(sample))
+
+    # output_file = '{}/{}.map.machine.reversed'.format(args.output_dir, game_name.split('.')[0])
+    # with open(output_file,'w', encoding='utf-8') as fout:
+    #     for item in map_reversed_list:
+    #         if item['act'] != None:
+    #             fout.write('{} (obj{}) --> {} --> {} (obj{}), step {}, desc: {}\n'.format(item['location_before'],
+    #                                                                                       item['location_before_id'],
+    #                                                                                       item['act'],
+    #                                                                                       item['location_after'],
+    #                                                                                       item['location_after_id'],
+    #                                                                                       item['step_num'],
+    #                                                                                       item['desc']))
+    #         else:
+    #             fout.write('\n')
 
     print ("Saved to {}".format(output_file))
     print ("Good Job!")
 
 
-
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--game_name", '-g', type=str)
+    parser.add_argument("--jericho_path", '-j', type=str, default="./data/z-machine-games-master/jericho-game-suite")
+    parser.add_argument("--max_steps", type=int, default=70)
+    parser.add_argument("--output_dir", '-odir', type=str, default="./data/maps")
+    parser.add_argument("--walk_md", '-md', action='store_true', help='toggle to output walkthrough.md, otherwise output walkthrough.txt')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    print ("Args: {}".format(args))
+    gen_move_machine(args)
