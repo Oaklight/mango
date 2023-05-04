9a960ca6b25cb1ce61b00af743d2cbe1f632bb81
Peng Ding <pding.dp@foxmail.com>
2023-05-04 23:57:33 +0800

    add arg message

--- scripts/gen_moves/run_gen_move_human_all.sh
+++ scripts/gen_moves/run_gen_move_human_all.sh
@@ -3,8 +3,23 @@
 # given as path of all jericho games folders
 # go over each folder and run the gen_move_human.py script on it
 
-path=${1:-'./data/maps'}
-jericho_env=${2:-'../z-machine-games-master/jericho-game-suite'}
+# path=${1:-'./data/maps'}
+# jericho_path=${2:-'../z-machine-games-master/jericho-game-suite'}
+# if not provided, prints the help message
+if [ $# -eq 0 ]; then
+    echo "Usage: ./run_gen_move_human_all.sh -p <path> -j <jericho_path>"
+    exit 1
+fi
+
+# parse the arguments
+while getopts p:j: flag
+do
+    case "${flag}" in
+        p) path=${OPTARG};;
+        j) jericho_path=${OPTARG};;
+    esac
+done
+
 games=$(ls $path)
 for game in $games
 do
@@ -13,7 +28,7 @@ do
     if [ ! -f $valid_moves ]; then
         continue
     else
-        python ./src/gen_moves/gen_move_human.py -c $valid_moves -j $jericho_env
+        python ./src/gen_moves/gen_move_human.py -c $valid_moves -j $jericho_path
     fi
 done
 
