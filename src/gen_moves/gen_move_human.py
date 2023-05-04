7508b7dd20ec16955b44ae7a15bfb1f2ed594cad
Peng Ding <pding.dp@foxmail.com>
2023-05-04 17:24:29 +0800

    gen_move_human.py and shell running all script

--- src/gen_moves/gen_move_human.py
+++ src/gen_moves/gen_move_human.py
@@ -1,30 +1,54 @@
 import argparse
+import csv
 import json
+import os
+from pathlib import Path
+from jericho import FrotzEnv
+
+direction_abbrv_dict = {
+    "e": "east",
+    "w": "west",
+    "n": "north",
+    "s": "south",
+    "ne": "northeast",
+    "nw": "northwest",
+    "se": "southeast",
+    "sw": "southwest",
+    "u": "up",
+    "d": "down",
+}  # jericho.defines.ABBRV_DICT
+direction_vocab_abbrv = direction_abbrv_dict.keys()
+direction_vocab = direction_abbrv_dict.values()
+opposite_direction_dict = {
+    "east": "west",
+    "west": "east",
+    "north": "south",
+    "south": "north",
+    "northeast": "southwest",
+    "southwest": "northeast",
+    "northwest": "southeast",
+    "southeast": "northwest",
+    "up": "up",
+    "down": "down",
+}
+
 
 # argparse to read in annotated walkthrough file, check file extension for either txt or markdown mode
 # output valid moves to json file
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("walkthrough_file", type=str, help="path to walkthrough file")
-    parser.add_argument(
-        "--csvOrjson", "-coj", type=str, default="csv", help="output to csv file"
-    )
-    args = parser.parse_args()
-
-    if args.walkthrough_file.endswith(".md"):
+def walkthrough_annotated_to_valid_moves(walkthrough_file, csv_or_json: str = "csv"):
+    if walkthrough_file.endswith(".md"):
         print("markdown mode")
-        args.isMarkdown = True
+        isMarkdown = True
     else:
         print("txt mode")
-        args.isMarkdown = False
-    SECTION_SPLITTER = "***" if args.isMarkdown else "==========="
-    SECTION_HEADER = "# " if args.isMarkdown else "==>"
-    TARGET_HEADER = "## " if args.isMarkdown else "==>"
+        isMarkdown = False
+    SECTION_SPLITTER = "***" if isMarkdown else "==========="
+    SECTION_HEADER = "# " if isMarkdown else "==>"
+    TARGET_HEADER = "## " if isMarkdown else "==>"
 
     valid_moves = {}
     # read in walkthrough file
-    with open(args.walkthrough_file, "r") as f:
+    with open(walkthrough_file, "r") as f:
         walkthrough = f.readlines()
 
         for line in walkthrough:
@@ -51,13 +75,11 @@ if __name__ == "__main__":
                     f"FOUND valid move [{step_num}]: {src_node} --> {action} --> {dst_node}"
                 )
 
-    print(f"{len(valid_moves)} valid moves found in {args.walkthrough_file}")
+    print(f"{len(valid_moves)} valid moves found in {walkthrough_file}")
     # dump valid moves to json file
     # output file name is game.valid_moves.json, game is part of walkthrough_file, in md mode, game.walkthrough.md, in txt mode, game.walkthrough
-    if args.csvOrjson == "csv":
-        output_file = (
-            args.walkthrough_file.split(".walkthrough")[0] + ".valid_moves.csv"
-        )
+    if csv_or_json == "csv":
+        output_file = walkthrough_file.split(".walkthrough")[0] + ".valid_moves.csv"
         with open(output_file, "w") as f:
             f.write("Step Num, Location Before, Location After\n")
             # write lines starting with step number, regardless of whether it's in valid_moves
@@ -71,87 +93,126 @@ if __name__ == "__main__":
                 else:
                     f.write(f"{i}, , \n")
 
-    elif args.csvOrjson == "json":
-        output_file = (
-            args.walkthrough_file.split(".walkthrough")[0] + ".valid_moves.json"
-        )
+    elif csv_or_json == "json":
+        output_file = walkthrough_file.split(".walkthrough")[0] + ".valid_moves.json"
         with open(output_file, "w") as f:
             json.dump(valid_moves, f, indent=4)
     else:
         raise NotImplementedError
+    print(f"Valid moves written to {output_file}")
+
+    return valid_moves
 
 
-# from jericho import * # https://jericho-py.readthedocs.io/en/latest/index.html
-# from argparse import ArgumentParser
-#
-#
-# direction_abbrv_dict = {'e': 'east', 'w': 'west', 'n': 'north', 's': 'south',
-#                         'ne': 'northeast', 'nw': 'northwest', 'se': 'southeast', 'sw': 'southwest',
-#                         'u': 'up', 'd': 'down'} # jericho.defines.ABBRV_DICT
-# direction_vocab_abbrv = direction_abbrv_dict.keys()
-# direction_vocab = direction_abbrv_dict.values()
-# opposite_direction_dict = {
-#     'east': 'west',
-#     'west': 'east',
-#     'north': 'south',
-#     'south': 'north',
-#     'northeast': 'southwest',
-#     'southwest': 'northeast',
-#     'northwest': 'southeast',
-#     'southeast': 'northwest',
-#     'up': 'up',
-#     'down': 'down'
-# }
-#
-# def read_csv(path_in, max_steps = 70):
-#     print ("Reading file {} ing ...".format(path_in))
-#     valid_moves = []
-#     with open(path_in, 'r', encoding='utf-8') as fin:
-#         lines = [line.strip() for line in fin.readlines()]
-#         print ("Head: {}".format(lines[0]))
-#         for line in lines[1:max_steps+1]:
-#             step_num, location_before, location_after = line.split(',')
-#             valid_moves.append({
-#                 'step_num': step_num.strip(),
-#                 'location_before': location_before.strip(),
-#                 'location_after': location_after.strip(),
-#                 'valid_move': (location_before.strip() != '')
-#             })
-#     print ("Done.")
-#     return valid_moves
-#
-# def main():
-#     parser = ArgumentParser()
-#     parser.add_argument("--jericho_path", '-j', type=str, default="z-machine-games-master/jericho-game-suite")
-#     parser.add_argument("--output_dir", '-odir', type=str, default="./maps")
-#     args = parser.parse_args()
-#
-#     game_name = 'zork1.z5'
-#     max_steps = 70
-#     print ('Game: {}, Max steps: {}'.format(game_name, max_steps))
-#
-#     env = FrotzEnv("{}/{}".format(args.jericho_path, game_name))
-#
-#     # walkthrough
-#     walkthrough = env.get_walkthrough()
-#     walkthrough = [direction_abbrv_dict[item.lower()] if item.lower() in direction_vocab_abbrv else item.lower() for item in walkthrough]
-#
-#     # annotated valid moves
-#     file_path = '{}/{}.valid_moves.csv'.format(args.output_dir, game_name.split('.')[0])
-#     print ("From {}".format(file_path))
-#     valid_moves = read_csv(file_path, max_steps= max_steps)
-#
-#     output_file = '{}/{}.map.human'.format(args.output_dir, game_name.split('.')[0])
-#     with open(output_file,'w', encoding='utf-8') as fout:
-#         for step_idx, move in enumerate(valid_moves):
-#             move['act'] = walkthrough[step_idx]
-#             if move['valid_move']:
-#                 fout.write('{} --> {} --> {}, step {}\n'.format(move['location_before'],
-#                                                                 move['act'],
-#                                                                 move['location_after'],
-#                                                                 move['step_num']))
-#     print ("Saved to {}".format(output_file))
-#     print ("Good Job!")
-#
-# if __name__ == '__main__':
-#     main()
+def read_csv(path_in, max_steps=70):
+    print("Reading file {} ing ...".format(path_in))
+    valid_moves = {}
+    with open(path_in, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        header = next(csv_reader)
+        step_num_idx = header.index("Step Num")
+        location_before_idx = header.index("Location Before")
+        location_after_idx = header.index("Location After")
+        for i, row in enumerate(csv_reader):
+            if i == max_steps:
+                break
+            step_num = row[step_num_idx].strip()
+            location_before = row[location_before_idx].strip()
+            location_after = row[location_after_idx].strip()
+            valid_move = location_before != ""
+            valid_moves[step_num] = {
+                "step_num": step_num,
+                "src_node": location_before,
+                "dst_node": location_after,
+                "valid_move": valid_move,
+            }
+    print("Done.")
+    return valid_moves
+
+
+def generate_move_human(valid_moves: dict, jericho_path, game_name, output_dir):
+    # get files in jericho_path, one of game_name.z3 or game_name.z5 or game_name.z8 must exist
+    jericho_files = os.listdir(jericho_path)
+    jericho_files = [
+        file for file in jericho_files if file.startswith(game_name)
+    ]  # len must be 1
+    jericho_path_for_game = os.path.join(jericho_path, jericho_files[0])
+
+    game_name = jericho_path_for_game.split("/")[-1].split(".")[0]
+    max_steps = min(len(valid_moves), 70)
+    print("Game: {}, Max steps: {}".format(game_name, max_steps))
+
+    # convert valid_moves to list, sorted by step number, int
+    valid_moves = [
+        valid_moves[key] for key in sorted(valid_moves.keys(), key=lambda x: int(x))
+    ]
+    valid_moves = valid_moves[:max_steps]
+    print(len(valid_moves))
+
+    env = FrotzEnv(jericho_path_for_game)
+
+    # walkthrough
+    walkthrough = env.get_walkthrough()
+    walkthrough = [
+        direction_abbrv_dict[item.lower()]
+        if item.lower() in direction_vocab_abbrv
+        else item.lower()
+        for item in walkthrough
+    ]
+
+    # annotated valid moves
+    output_file = "{}/{}.map.human".format(args.output_dir, game_name)
+    with open(output_file, "w", encoding="utf-8") as fout:
+        for step_idx, move in enumerate(valid_moves):
+            try:
+                move["act"] = walkthrough[step_idx]
+            except Exception as e:
+                print(e)
+                print(
+                    "step_idx: {}, len(walkthrough): {}".format(
+                        step_idx, len(walkthrough)
+                    )
+                )
+            if move["valid_move"]:
+                fout.write(
+                    "{} --> {} --> {}, step {}\n".format(
+                        move["src_node"],
+                        move["act"],
+                        move["dst_node"],
+                        move["step_num"],
+                    )
+                )
+    print("Saved to {}".format(output_file))
+    print("Good Job!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # walkthrough_file, valid_move_csv as mutually exclusive
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--walkthrough_file", "-w", type=str, default=None)
+    group.add_argument("--valid_move_csv", "-c", type=str, default=None)
+
+    parser.add_argument(
+        "--jericho_path",
+        "-j",
+        type=str,
+        default="z-machine-games-master/jericho-game-suite",
+    )
+    args = parser.parse_args()
+    # output_dir is the same folder as walkthrough_file: ../../data/maps/omniquest/omniquest.valid_moves.csv
+    # if walkthrough_file is not provided, then output_dir is the same folder as valid_move_csv
+    if args.walkthrough_file:
+        args.output_dir = "/".join(args.walkthrough_file.split("/")[:-1])
+        args.game_name = args.walkthrough_file.split("/")[-1].split(".")[0]
+    else:
+        args.output_dir = "/".join(args.valid_move_csv.split("/")[:-1])
+        args.game_name = args.valid_move_csv.split("/")[-1].split(".")[0]
+
+    if args.valid_move_csv:
+        valid_moves = read_csv(args.valid_move_csv)
+    else:
+        valid_moves = walkthrough_annotated_to_valid_moves(args.walkthrough_file)
+
+    # jericho_path_for_game = args.jericho_path + '/' + args.game_name + '.z5'
+    generate_move_human(valid_moves, args.jericho_path, args.game_name, args.output_dir)
