565c68b989889a61944d82ccbd3605a49586e78e
Peng Ding <dingpeng@uchicago.edu>
2023-05-28 03:28:39 +0800

    add argparse for scripts

--- scripts/gen_moves/run_gen_move_machine_all.sh
+++ scripts/gen_moves/run_gen_move_machine_all.sh
@@ -2,13 +2,26 @@
 
 #lgop.z3 unsupportedGameWarning
 #theatre env.get_player_location() == None
-path=${1:-'./data/z-machine-games-master/jericho-game-suite'}
-files=$(ls $path)
+# path=${1:-'./data/z-machine-games-master/jericho-game-suite'}
+if [ $# -eq 0 ]; then
+    echo "Usage: ./run_gen_move_machine_all.sh -j <jericho_path>"
+    exit 1
+fi
+
+# parse the arguments
+while getopts j: flag
+do
+    case "${flag}" in
+        j) jericho_path=${OPTARG};;
+    esac
+done
+
+files=$(ls $jericho_path)
 for filename in $files
 do
    echo $filename
    python ./src/gen_moves/gen_move_machine.py -g $filename \
-   -j ./data/z-machine-games-master/jericho-game-suite \
+   -j $jericho_path \
    --max_steps 70 \
    -odir ./data/maps
 done
--- scripts/gen_moves/run_gen_walkthrough_all.sh
+++ scripts/gen_moves/run_gen_walkthrough_all.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 #lgop.z3 error
-path=${1:-'./data/z-machine-games-master/jericho-game-suite'}
-files=$(ls $path)
+# path=${1:-'./data/z-machine-games-master/jericho-game-suite'}
+if [ $# -eq 0 ]; then
+    echo "Usage: ./run_gen_walkthrough_all.sh -j <jericho_path>"
+    exit 1
+fi
+
+# parse the arguments
+while getopts j: flag
+do
+    case "${flag}" in
+        j) jericho_path=${OPTARG};;
+    esac
+done
+
+files=$(ls $jericho_path)
 for filename in $files
 do
    echo $filename
    python ./src/gen_moves/gen_walkthrough.py -g $filename \
-   -j ./data/z-machine-games-master/jericho-game-suite \
+   -j $jericho_path \
    -odir ./data/maps
 done
 
