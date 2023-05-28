93b5eda9aea7ee974c97dd41f9aa1f9fe6a07f32
Peng Ding <dingpeng@uchicago.edu>
2023-05-28 17:08:32 +0800

    sort to stable order btw runs

--- src/gen_moves/gen_move_final.py
+++ src/gen_moves/gen_move_final.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import os
+from collections import OrderedDict
 
 
 # helper function to get such dict
@@ -21,9 +22,9 @@ def get_dict(lines):
         path_str, step_str = line.split(", step ")
         step = int(step_str)
         elements = path_str.split(" --> ")
-        src = elements[0]
-        act = elements[1]
-        dst = elements[2]
+        src = elements[0].strip()
+        act = elements[1].strip()
+        dst = elements[2].strip()
         d[step] = {"src": src, "dst": dst, "act": act}
     return d
 
@@ -60,16 +61,23 @@ def get_args():
     return args
 
 
-def sanity_check(machine_dict, human_dict, walkthrough_file):
+def sanity_check(machine_dict, human_dict):
     """
     compare machine_dict with human_dict
     - get common step numbers, appear in both machine and human
     - get diff step numbers, appear in machine but not human
     - get diff step numbers, appear in human but not machine
     """
-    common_steps = set(machine_dict.keys()).intersection(set(human_dict.keys()))
-    machine_only_steps = set(machine_dict.keys()).difference(set(human_dict.keys()))
-    human_only_steps = set(human_dict.keys()).difference(set(machine_dict.keys()))
+    # sort step number list for stable order
+    common_steps = sorted(
+        list(set(machine_dict.keys()).intersection(set(human_dict.keys())))
+    )
+    machine_only_steps = sorted(
+        list(set(machine_dict.keys()).difference(set(human_dict.keys())))
+    )
+    human_only_steps = sorted(
+        list(set(human_dict.keys()).difference(set(machine_dict.keys())))
+    )
     # print to notify user
     print(
         "common steps: {}, machine only steps: {}, human only steps: {}".format(
@@ -81,13 +89,20 @@ def sanity_check(machine_dict, human_dict, walkthrough_file):
     # wait for signal, Y/n, Y as default, check to see if user wants to continue
     # if not, exit
     # if yes, continue
-    while len(machine_only_steps) > 0 or len(human_only_steps) > 0:
-        signal = input("Continue? [y/n] ").strip()
-        if signal == "n":
-            print("Please manually review difference\n")
-            exit()
-        if signal == "y":
-            return
+    # return step sets for further processing
+    if len(machine_only_steps) == 0 and len(human_only_steps) == 0:
+        print("No difference found, exiting...")
+        return common_steps, machine_only_steps, human_only_steps
+    else:
+        while True:
+            signal = input("Continue? [y/n] ").strip()
+            if signal == "n":
+                print("Please manually review difference\n")
+                exit()
+            elif signal == "y":
+                return common_steps, machine_only_steps, human_only_steps
+            else:
+                print("Invalid input, please enter y or n")
 
 
 def load_both_maps(get_dict, args):
@@ -110,37 +125,65 @@ if __name__ == "__main__":
     # go over each line in game.map.machine, grab nodeCode and find the corresponding node name in game.map.human
     machine_dict, human_dict = load_both_maps(get_dict, args)
 
-    sanity_check(machine_dict, human_dict, args.walkthrough)
+    common_steps, machine_only_steps, human_only_steps = sanity_check(
+        machine_dict, human_dict
+    )
     print("Reload both maps after resolution")
     machine_dict, human_dict = load_both_maps(get_dict, args)
 
-    code2anno = {}
-    anno2code = {}
-    # iter over machine dict, find the corresponding human line
-    for step_num in machine_dict.keys():
-        src_code = machine_dict[step_num]["src"]
+    code2anno = OrderedDict()
+    anno2code = OrderedDict()
+
+    """
+    assumption: human annotation is a connected graph, each line is a edge. Thus, the end node of current line must be the start node of next line. I only need to check matching of end node of each line. and start node of the first line.
+    """
+    # first process the common_steps, which are the same in both machine and human
+    for i, step_num in enumerate(common_steps):
         dst_code = machine_dict[step_num]["dst"]
-        src_anno = human_dict[step_num]["src"]
         dst_anno = human_dict[step_num]["dst"]
-
-        code2anno[src_code] = src_anno
         code2anno[dst_code] = dst_anno
-
-        if src_anno not in anno2code:
-            anno2code[src_anno] = set([src_code])
-        else:
-            anno2code[src_anno].add(src_code)
         if dst_anno not in anno2code:
             anno2code[dst_anno] = set([dst_code])
         else:
             anno2code[dst_anno].add(dst_code)
+
+        if i == 0:
+            # first line, start node is the start node of the first line
+            src_code = machine_dict[step_num]["src"]
+            src_anno = human_dict[step_num]["src"]
+            code2anno[src_code] = src_anno
+            if src_anno not in anno2code:
+                anno2code[src_anno] = set([src_code])
+            else:
+                anno2code[src_anno].add(src_code)
+
+    # then process the human_only_steps, which are annotated by human but not machine
+    for step_num in human_only_steps:
+        # since there is no matching machine code, we can only see if there was a previous step
+        src_anno = human_dict[step_num]["src"]
+        dst_anno = human_dict[step_num]["dst"]
+
+        if src_anno not in anno2code:
+            prompt = "Please enter the code for {} (no previous step): ".format(
+                src_anno
+            )
+            src_code = input(prompt).strip()
+            code2anno[src_code] = src_anno
+            anno2code[src_anno] = set([src_code])
+        if dst_anno not in anno2code:
+            prompt = "Please enter the code for {} (no previous step): ".format(
+                dst_anno
+            )
+            dst_code = input(prompt).strip()
+            code2anno[dst_code] = dst_anno
+            anno2code[dst_anno] = set([dst_code])
 
     # cast anno2code entry from set to list
     for anno in anno2code.keys():
         anno2code[anno] = list(anno2code[anno])
         # sort anno2code entry
         anno2code[anno].sort()
-        
+
     # write to json file
     with open(
         os.path.join(args.output_path, f"{args.game_name}.code2anno.json"), "w"
