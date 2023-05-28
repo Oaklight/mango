8a62da4457e1fbfd0fd6bd4cc4bb8261697b970d
Peng Ding <dingpeng@uchicago.edu>
2023-05-29 04:04:18 +0800

    update reverse map by cross checking with human forward map entries for same set of nodes

--- scripts/gen_moves/run_gen_move_reversed_all.sh
+++ scripts/gen_moves/run_gen_move_reversed_all.sh
@@ -2,27 +2,46 @@
 
 # path=${1:-'./data/z-machine-games-master/jericho-game-suite'}
 if [ $# -eq 0 ]; then
-    echo "Usage: ./run_gen_move_reversed_all.sh -j <jericho_path>"
+    echo "Usage: ./run_gen_move_reversed_all.sh -j <jericho_path> [-g <game_name>]"
     exit 1
 fi
 
 # parse the arguments
-while getopts j: flag
+# while getopts j: flag
+# do
+#     case "${flag}" in
+#         j) jericho_path=${OPTARG};;
+#     esac
+# done
+while getopts j:g: flag
 do
     case "${flag}" in
         j) jericho_path=${OPTARG};;
+        g) game_name=${OPTARG};;
     esac
 done
 
-files=$(ls $jericho_path)
-for filename in $files
-do
-   echo $filename
-   python ./src/gen_moves/gen_move_reversed.py -g $filename \
-   -j $jericho_path \
-   --max_steps 70 \
-   -idir ./data/maps \
-   -odir ./data/maps
-done
-
+# if g not provided, generate for all games
+if [ -z "$game_name" ]
+then
+    echo "Generating for all games..."
+    files=$(ls $jericho_path)
+    for filename in $files
+    do
+        echo $filename
+        python ./src/gen_moves/gen_move_reversed.py -g $filename \
+        -j $jericho_path \
+        --max_steps 70 \
+        -idir ./data/maps \
+        -odir ./data/maps
+    done
+    echo "Good Job!"
+fi
+# generate for a specific game
+echo "Generating for $game_name..."
+python ./src/gen_moves/gen_move_reversed.py -g $game_name \
+-j $jericho_path \
+--max_steps 70 \
+-idir ./data/maps \
+-odir ./data/maps
 echo "Good Job!"
