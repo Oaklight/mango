8034b29a35ad2e9708173a07331faff71a8ad6c8
Peng Ding <dingpeng@uchicago.edu>
2023-05-30 13:58:46 +0800

    all2all script with data release

--- scripts/gen_paths/run_gen_all2all.sh
+++ scripts/gen_paths/run_gen_all2all.sh
@@ -34,3 +34,48 @@ else
     output_dir="$map_path/$game"
     python src/gen_paths/gen_all2all.py -m $map_human -r $map_reversed -odir $output_dir
 fi
+
+
+# GAME RELEASE
+# if g not specified, release all games
+if [ -z "$game" ]
+then
+    for game in $games
+    do
+        # release by copy game.all2all.json, game.code2anno.json, game.anno2code.json, game.walkthrough, game.moves to data/maps/game to release folder
+        release_dir="./data/maps-release"
+        mkdir -p $release_dir
+        # go over each dir in map_path, check if all release file present, then release
+        if [ -f "$map_path/$game/$game.all2all.json" ] && [ -f "$map_path/$game/$game.code2anno.json" ] && [ -f "$map_path/$game/$game.anno2code.json" ] && [ -f "$map_path/$game/$game.walkthrough" ] && [ -f "$map_path/$game/$game.moves" ]
+        then
+            game_release_dir="$release_dir/$game"
+            mkdir -p $game_release_dir
+            cp "$map_path/$game/$game.all2all.json" "$game_release_dir/$game.all2all.json"
+            cp "$map_path/$game/$game.code2anno.json" "$game_release_dir/$game.code2anno.json"
+            cp "$map_path/$game/$game.anno2code.json" "$game_release_dir/$game.anno2code.json"
+            cp "$map_path/$game/$game.walkthrough" "$game_release_dir/$game.walkthrough"
+            cp "$map_path/$game/$game.moves" "$game_release_dir/$game.moves"
+            echo "Release $game"
+        else
+            echo "Missing release files for $game"
+        fi
+    done
+else
+    # release by copy game.all2all.json, game.code2anno.json, game.anno2code.json, game.walkthrough, game.moves to data/maps/game to release folder
+    release_dir="./data/maps-release"
+    mkdir -p $release_dir
+    # go over each dir in map_path, check if all release file present, then release
+    if [ -f "$map_path/$game/$game.all2all.json" ] && [ -f "$map_path/$game/$game.code2anno.json" ] && [ -f "$map_path/$game/$game.anno2code.json" ] && [ -f "$map_path/$game/$game.walkthrough" ] && [ -f "$map_path/$game/$game.moves" ]
+    then
+        game_release_dir="$release_dir/$game"
+        mkdir -p $game_release_dir
+        cp "$map_path/$game/$game.all2all.json" "$game_release_dir/$game.all2all.json"
+        cp "$map_path/$game/$game.code2anno.json" "$game_release_dir/$game.code2anno.json"
+        cp "$map_path/$game/$game.anno2code.json" "$game_release_dir/$game.anno2code.json"
+        cp "$map_path/$game/$game.walkthrough" "$game_release_dir/$game.walkthrough"
+        cp "$map_path/$game/$game.moves" "$game_release_dir/$game.moves"
+        echo "Release $game"
+    else
+        echo "Missing release files for $game"
+    fi
+fi
