a31f6b90a053f77a18593848c6a201f6d9d90a58
Peng Ding <dingpeng@uchicago.edu>
2023-06-03 01:21:25 +0800

    diff all_pairs btw fjd and new version

--- src/gen_paths/gen_all2all.py
+++ src/gen_paths/gen_all2all.py
@@ -31,18 +31,20 @@ if __name__ == "__main__":
     # all_pairs = list(itertools.permutations(g.nodes(), 2))
     # print(all_pairs)
 
-    all_pairs_dict = {}
+    all_pairs_dict = []
     all_paths_json = []
     for src_node, dst_node in all_pairs:
         print(f"Generating paths from {src_node} to {dst_node}...")
         allPaths = get_all_paths(g, src=src_node, dst=dst_node)
         all_paths_json += get_all_paths_json(g, allPaths, diff_shortest=True)
 
-        all_pairs_dict[src_node + " -> " + dst_node] = {
-            "src_node": src_node,
-            "dst_node": dst_node,
-            "num_paths": len(allPaths),
-        }
+        all_pairs_dict.append(
+            {
+                "src_node": src_node,
+                "dst_node": dst_node,
+                "num_paths": len(allPaths),
+            }
+        )
 
     # sort by src, dst, diff_shortest, step_count
     # additionally sort by elements in path_details
@@ -73,10 +75,11 @@ if __name__ == "__main__":
         os.path.dirname(args.output_path),
         args.map.split("/")[-1].split(".")[0] + ".all_pairs.json",
     )
-    # sort by src, dst, num_paths
+
     all_pairs_dict = sorted(
-        all_pairs_dict.items(),
-        key=lambda x: (x[1]["src_node"], x[1]["dst_node"], x[1]["num_paths"]),
+        all_pairs_dict,
+        key=lambda x: (x["src_node"], x["dst_node"], x["num_paths"]),
     )
+
     with open(pair_json_path, "w") as f:
         json.dump(all_pairs_dict, f, indent=4)
