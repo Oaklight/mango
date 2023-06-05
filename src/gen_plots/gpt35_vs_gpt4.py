8fc0b32646cf67e367be818f860c6051a488677c
Peng Ding <dingpeng@uchicago.edu>
2023-06-06 03:40:12 +0800

    plot vs figure for DF and RF

--- src/gen_plots/gpt35_vs_gpt4.py
+++ src/gen_plots/gpt35_vs_gpt4.py
@@ -8,36 +8,6 @@ import numpy as np
 TEMP_DIR = "./src/gen_plots/data"
 EVAL_DIR = "./evals"
 GLOBAL_FONTSIZE = 13
-# read in data from TEMP_DIR for each model
-
-# model1: gpt3.5, model2: gpt4
-model1_nice_scores = []
-model1_harsh_scores = []
-model2_nice_scores = []
-model2_harsh_scores = []
-
-# read in data from TEMP_DIR for each model
-model1_real_path_route = TEMP_DIR + "/gpt3.5/route.json"
-model2_real_path_route = TEMP_DIR + "/gpt4/route.json"
-model1_real_path_desti = TEMP_DIR + "/gpt3.5/desti.json"
-model2_real_path_desti = TEMP_DIR + "/gpt4/desti.json"
-
-model1_real_route = json.load(open(model1_real_path_route))
-model2_real_route = json.load(open(model2_real_path_route))
-model1_real_desti = json.load(open(model1_real_path_desti))
-model2_real_desti = json.load(open(model2_real_path_desti))
-
-# get game names, anything other than "avg_harsh" and "avg_nice"
-game_names = [
-    key for key in model1_real_route.keys() if key not in ["avg_harsh", "avg_nice"]
-]
-
-# get nice and harsh scores for each game
-for game_name in game_names:
-    model1_nice_scores.append(model1_real_route[game_name]["nice"])
-    model1_harsh_scores.append(model1_real_route[game_name]["harsh"])
-    model2_nice_scores.append(model2_real_route[game_name]["nice"])
-    model2_harsh_scores.append(model2_real_route[game_name]["harsh"])
 
 
 # plot gpt4 vs gpt3.5 avg acc for each task, gpt3.5 as x axis, gpt4 as y axis, and a trend line y=x, add game name to each point
@@ -65,19 +35,66 @@ def plot_vs(savepath, font_size, model1_nice_scores, model2_nice_scores, game_na
     plt.show()
 
 
-# nice version
-plot_vs(
-    EVAL_DIR + "/gpt35_vs_gpt4_nice.png",
-    GLOBAL_FONTSIZE,
-    model1_nice_scores,
-    model2_nice_scores,
-    game_names,
+def plot_for_task(model1_real_path, model2_real_path, task="desti"):
+    assert task in ["desti", "route"], "task must be desti or route"
+    assert task in model1_real_path, "task must match model1_real_path"
+    assert task in model2_real_path, "task must match model2_real_path"
+
+    # model1: gpt3.5, model2: gpt4
+    model1_nice_scores = []
+    model1_harsh_scores = []
+    model2_nice_scores = []
+    model2_harsh_scores = []
+
+    model1_real = json.load(open(model1_real_path))
+    model2_real = json.load(open(model2_real_path))
+
+    # get game names, anything other than "avg_harsh" and "avg_nice"
+    game_names = [
+        key for key in model1_real.keys() if key not in ["avg_harsh", "avg_nice"]
+    ]
+
+    # get nice and harsh scores for each game
+    for game_name in game_names:
+        model1_nice_scores.append(model1_real[game_name]["nice"])
+        model1_harsh_scores.append(model1_real[game_name]["harsh"])
+        model2_nice_scores.append(model2_real[game_name]["nice"])
+        model2_harsh_scores.append(model2_real[game_name]["harsh"])
+
+    # nice version
+    plot_vs(
+        EVAL_DIR + f"/gpt35_vs_gpt4.{task}.nice.png",
+        GLOBAL_FONTSIZE,
+        model1_nice_scores,
+        model2_nice_scores,
+        game_names,
+    )
+    # harsh version
+    plot_vs(
+        EVAL_DIR + f"/gpt35_vs_gpt4.{task}.harsh.png",
+        GLOBAL_FONTSIZE,
+        model1_harsh_scores,
+        model2_harsh_scores,
+        game_names,
+    )
+
+
+model1_real_path_desti = TEMP_DIR + "/gpt3.5/desti.json"
+model2_real_path_desti = TEMP_DIR + "/gpt4/desti.json"
+
+model1_real_path_route = TEMP_DIR + "/gpt3.5/route.json"
+model2_real_path_route = TEMP_DIR + "/gpt4/route.json"
+
+# plot for route task
+plot_for_task(
+    model1_real_path_route,
+    model2_real_path_route,
+    task="route",
 )
-# harsh version
-plot_vs(
-    EVAL_DIR + "/gpt35_vs_gpt4_harsh.png",
-    GLOBAL_FONTSIZE,
-    model1_harsh_scores,
-    model2_harsh_scores,
-    game_names,
+
+# plot for destination task
+plot_for_task(
+    model1_real_path_desti,
+    model2_real_path_desti,
+    task="desti",
 )
