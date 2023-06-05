508986838d188b3b467565865d9a97d31fc959d9
Peng Ding <dingpeng@uchicago.edu>
2023-06-06 03:23:05 +0800

    lightcoral dotted y=x; bigger translucent dot with upper right horizontal text

--- src/gen_plots/gpt35_vs_gpt4.py
+++ src/gen_plots/gpt35_vs_gpt4.py
@@ -7,6 +7,7 @@ import numpy as np
 
 TEMP_DIR = "./src/gen_plots/data"
 EVAL_DIR = "./evals"
+GLOBAL_FONTSIZE = 13
 # read in data from TEMP_DIR for each model
 
 # model1: gpt3.5, model2: gpt4
@@ -38,13 +39,45 @@ for game_name in game_names:
     model2_nice_scores.append(model2_real_route[game_name]["nice"])
     model2_harsh_scores.append(model2_real_route[game_name]["harsh"])
 
+
+# plot gpt4 vs gpt3.5 avg acc for each task, gpt3.5 as x axis, gpt4 as y axis, and a trend line y=x, add game name to each point
+def plot_vs(savepath, font_size, model1_nice_scores, model2_nice_scores, game_names):
+    plt.figure(figsize=(10, 10))
+    plt.tight_layout()
+    plt.scatter(model1_nice_scores, model2_nice_scores, s=200, alpha=0.5)
+    plt.plot([0, 1], [0, 1], color="lightcoral", linestyle="--", alpha=0.8)
+
+    # add text to each point
+    for i, game_name in enumerate(game_names):
+        plt.annotate(
+            game_name,
+            (model1_nice_scores[i], model2_nice_scores[i]),
+            xytext=(8, 8),
+            textcoords="offset points",
+            ha="left",
+            va="top",
+            fontsize=10,
+        )
+    plt.xlabel("gpt3.5", fontsize=font_size)
+    plt.ylabel("gpt4", fontsize=font_size)
+    plt.xticks(fontsize=font_size)
+    plt.savefig(savepath, dpi=300, bbox_inches="tight")
+    plt.show()
+
+
 # nice version
-# plot gpt4 vs gpt3.5 avg acc for each task, gpt3.5 as x axis, gpt4 as y axis, and a trend line y=x
-plt.figure(figsize=(10, 10))
-plt.scatter(model1_nice_scores, model2_nice_scores)
-plt.plot([0, 1], [0, 1], color="red")
-plt.xlabel("gpt3.5")
-plt.ylabel("gpt4")
-plt.title("nice accuracy")
-plt.savefig(EVAL_DIR + "/gpt35_vs_gpt4_nice.png")
-plt.show()
+plot_vs(
+    EVAL_DIR + "/gpt35_vs_gpt4_nice.png",
+    GLOBAL_FONTSIZE,
+    model1_nice_scores,
+    model2_nice_scores,
+    game_names,
+)
+# harsh version
+plot_vs(
+    EVAL_DIR + "/gpt35_vs_gpt4_harsh.png",
+    GLOBAL_FONTSIZE,
+    model1_harsh_scores,
+    model2_harsh_scores,
+    game_names,
+)
