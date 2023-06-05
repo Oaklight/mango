354e9c2d329d24185c859fa7a16ef7663a85434d
Peng Ding <dingpeng@uchicago.edu>
2023-06-05 11:38:23 +0800

    tune color

--- src/gen_plots/colors.py
+++ src/gen_plots/colors.py
@@ -1,5 +1,6 @@
 COLOR_MAP = {
-    "gpt4": "#4682B4",  # steel blue (dark blue)
+    # "gpt4": "#4682B4",  # steel blue (dark blue)
+    "gpt4": "#0000FF",  # blue (blue)
     "gpt3.5": "#6495ED",  # cornflower blue (light blue)
     "rwkv": "#50C878",  # emerald green (light green)
     "llama": "#FFD700",  # gold (yellow)
