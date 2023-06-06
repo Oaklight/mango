565f176ad0338feb3c1ad782319c6e8d1e49acc4
Peng Ding <dingpeng@uchicago.edu>
2023-06-06 09:46:38 +0800

    code for "skip eval due to cutoff"

--- scripts/gen_evals/run_eval_route.sh
+++ scripts/gen_evals/run_eval_route.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# python src/gen_evals/gpt.eval_df_simple.py -r ../gpt-games-results/dragon/results/ --simple -odir ./evals/gpt -g dragon
 if [ $# -eq 0 ]; then
-    echo "Usage: ./run_eval_route.harsh.sh -m <model_name> -p <path_to_model_results> -g <game> <--harsh || --nice>"
+    echo "Usage: ./run_eval_route.harsh.sh -m <model_name> -p <path_to_model_results> -g <game> -c <cutoff_json> <-h(ash) || -n(ice)>"
     exit 1
 fi
 
 # parse the arguments
-while getopts ":m:p:g:nh" opt; do
+while getopts ":m:p:g:c:hn" opt; do
     case $opt in
         m) model_name="$OPTARG";;
         p) path_to_model_results="$OPTARG";;
         g) game="$OPTARG";;
-        n) nice=true;;
+        c) cutoff_json="$OPTARG";;
         h) harsh=true;;
+        n) nice=true;;
         \?) echo "Invalid option -$OPTARG" >&2;;
     esac
 done
@@ -24,20 +24,20 @@ then
     for game in $games
     do
         # run eval on all games
-        if [ -z "$nice" ]
+        if [ -z "$nice" ] # if nice is not set
         then
-            python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ -odir ./evals/$model_name/route -g $game
+            python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ -odir ./evals/$model_name/route -c $cutoff_json -g $game
         else
-            python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ --simple -odir ./evals/$model_name/route -g $game
+            python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ --simple -odir ./evals/$model_name/route -c $cutoff_json -g $game
         fi
     done
 else
     # run eval on specific game
-    if [ -z "$nice" ]
+    if [ -z "$nice" ] # if nice is not set
     then
-        python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ -odir ./evals/$model_name/route -g $game
+        python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ -odir ./evals/$model_name/route -c $cutoff_json -g $game
     else
-        python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ --simple -odir ./evals/$model_name/route -g $game
+        python src/gen_evals/$model_name.eval_route.py -r $path_to_model_results/$game/results/ --simple -odir ./evals/$model_name/route -c $cutoff_json -g $game
     fi
 fi
 
