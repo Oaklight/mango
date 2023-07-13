cbea10e68398f1b1107f5c78ff8b5d050271fb8a
Peng Ding <dingpeng@uchicago.edu>
2023-07-13 15:51:43 +0800

    no_confirm flag for all2all generation scripts

--- src/gen_paths/utils.py
+++ src/gen_paths/utils.py
@@ -101,6 +101,7 @@ def get_args_all2all():
 
     parser.add_argument("--output_dir", "-odir", type=str)
     parser.add_argument("--node_step_map", "-nsm", type=str)
+    parser.add_argument("--no_confirm", "-nc", action="store_true")
     args = parser.parse_args()
 
     # output_dir is default to be the same as map file
