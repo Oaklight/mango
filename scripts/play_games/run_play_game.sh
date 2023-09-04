c2d3973f4188d3b01f8e2e457288dc48e66710ee
dasepli <ruhao9805@gmail.com>
2023-09-05 01:26:45 +0800

    add play game results

--- scripts/play_games/run_play_game.sh
+++ scripts/play_games/run_play_game.sh
@@ -1,2 +1,2 @@
 # "gpt-4", "gpt-3.5-turbo"
-python src/play_games/play_game.py --model_name "gpt-3.5-turbo" --game_name "pentari.z5" --jericho_path "./z-machine-games-master/jericho-game-suite" --cache_dir "src/play_games/cache" --max_steps 120
+python src/play_games/play_game.py --model_name "gpt-3.5-turbo" --game_name "pentari.z5" --jericho_path "./z-machine-games-master/jericho-game-suite" --cache_dir "src/play_games/cache" --max_steps 120 --aug_map
--- src/play_games/play_game.py
+++ src/play_games/play_game.py
@@ -7,9 +7,26 @@ import time
 import argparse
 import json
 from tqdm import tqdm
+from collections import defaultdict
 
 from jericho import *
 from src.play_games.api import api_complete
+
+
+direction_list = ['east', 'west', 'north', 'south', 'northeast', 'northwest', 'southeast', 'up', 'down']
+opposite_direction_dict = {
+    'east': 'west',
+    'west': 'east',
+    'north': 'south',
+    'south': 'north',
+    'northeast': 'southwest',
+    'southwest': 'northeast',
+    'northwest': 'southeast',
+    'southeast': 'northwest',
+    'up': 'up',
+    'down': 'down'
+}
+
 
 def play_game(args):
     model_name = args.model_name
@@ -17,6 +34,7 @@ def play_game(args):
     max_steps = args.max_steps
     env = FrotzEnv("{}/{}".format(args.jericho_path, game_name))
 
+    map_adjacent_dict = defaultdict(dict)
     system_prompt = "Your task is to play an interactive fiction game. Please try to get as many points as possible."
     history_steps = ""
     reward = 0
@@ -27,15 +45,25 @@ def play_game(args):
         'observation': observation,
         'reward': reward,
         'done': False,
-        'info': info
+        'info': info,
+        'prompt': ''
     }
     walkthrough_list = []
     walkthrough_list.append(act_info)
+    location_tmp = 'Init'
 
     for step_num in range(max_steps):
         print ("==> step num: ", step_num)
         valid_actions = env.get_valid_actions()
-        prompt = '- history: {}\n- new observation: {}\n- valid actions: {}\nPlease select an action from the valid actions list. Please just tell me the selected action without any extra words.'.format(history_steps, observation,valid_actions)
+        current_map = ''
+        if args.aug_map:
+            if location_tmp in map_adjacent_dict:
+                current_map = '\n- extra info: ' # '\n- hints: '
+                current_map_dict = map_adjacent_dict[location_tmp]
+                for direction in current_map_dict:
+                    current_map += 'If you go {}, you will get to "{}";'.format(direction, current_map_dict[direction])
+
+        prompt = '- history: {}\n- new observation: {}{}\n- valid actions: {}\nPlease select an action from the valid actions list. Please just tell me the selected action without any extra words.'.format(history_steps, observation, current_map, valid_actions)
         try:
             act = api_complete(system_prompt=system_prompt, prompt = prompt, model=model_name).strip().strip('"').strip("'")
         except Exception as e:
@@ -43,6 +71,12 @@ def play_game(args):
             break
         history_steps += '\nobservation: {}\nselected action: "{}"\nreward: {}\n'.format(observation, act, reward)
         observation, reward, done, info = env.step(act)
+        if args.aug_map and act in set(direction_list):
+            location_new = observation.strip().split('\n')[0].strip()  # ??
+            map_adjacent_dict[location_tmp][act] = location_new
+            map_adjacent_dict[location_new][opposite_direction_dict[act]] = location_tmp
+            location_tmp = location_new
+
         sample_info = {
             'valid_actions': valid_actions,
             'act': act,
@@ -50,6 +84,7 @@ def play_game(args):
             'reward': reward,
             'done': done,
             'info': info,
+            'prompt': prompt
         }
         walkthrough_list.append(sample_info)
         print (sample_info)
@@ -59,7 +94,11 @@ def play_game(args):
     cache_dir = os.path.join(args.cache_dir, game_name.split('.')[0])
     if os.path.exists(cache_dir) == False:
         os.makedirs(cache_dir)
-    outfile = '{}/{}_walkthrough.json'.format(cache_dir, model_name)
+
+    aug_map = ''
+    if args.aug_map:
+        aug_map = 'aug_map'
+    outfile = '{}/{}_{}_walkthrough.json'.format(cache_dir, model_name, aug_map)
     with open(outfile, 'w+', encoding='utf-8') as fout:
         fout.write(json.dumps(walkthrough_list, indent=4))
     print ('Well Done!')
@@ -69,6 +108,7 @@ def parse_args():
     parser.add_argument("--model_name", '-m', type=str, default="gpt-3.5-turbo")
     parser.add_argument("--game_name", '-g', type=str, default="zork1")
     parser.add_argument("--jericho_path", '-j', type=str, default="./z-machine-games-master/jericho-game-suite")
+    parser.add_argument("--aug_map", action='store_true')
     parser.add_argument("--cache_dir", '-cdir', type=str, default="./data/maps")
     parser.add_argument("--max_steps", type=int, default=120)
     return parser.parse_args()
